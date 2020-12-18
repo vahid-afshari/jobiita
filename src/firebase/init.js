3cfab9008dadf84a55aafc5411bfbef27af54a63
Vahid Afshari <afshari.vahid@gmail.com>
2020-12-18 18:15:36 +0200

    Update init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'
 import firestore from 'firebase/firestore'
 var config = {
-    apiKey: "AIzaSyBCGiNRHC9oEwTGo-VXwBZ1A5eB6E5eRe8",
-    authDomain: "jobitta-form-talent.firebaseapp.com",
+    apiKey: "",
+    authDomain: "",
     databaseURL: "https://jobitta-form-talent.firebaseio.com",
     projectId: "jobitta-form-talent",
     storageBucket: "jobitta-form-talent.appspot.com",
-    messagingSenderId: "38006004589",
-    appId: "1:38006004589:web:2297fb09ddbedf583ad864"
+    messagingSenderId: "",
+    appId: ""
   };
   // Initialize Firebase
   const firebaseApp = firebase.initializeApp(config); 
